0237bb4505b1521a49446ef645e7429ebdea13f4
kopniiiin <kopniiiin@icloud.com>
2020-07-12 16:33:51 +0300

    Добавляет тесты для HOC

---
+++ src/hocs/with-active-item/with-active-item.e2e.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {shallow} from "enzyme";
+
+import withActiveItem from "./with-active-item.jsx";
+
+const MockComponent = () => <div/>;
+const MockComponentWithActiveItem = withActiveItem(MockComponent);
+
+describe(`e2e test: withActiveItem HOC`, () => {
+  it(`should pass activeItem`, () => expect(
+      shallow(<MockComponentWithActiveItem/>).props().activeItem
+  ).toBe(null));
+
+  it(`should change activeItem`, () => {
+    const activeItem = `4`;
+    const wrapper = shallow(<MockComponentWithActiveItem/>);
+    wrapper.props().onActiveItemChange(activeItem);
+    expect(wrapper.props().activeItem).toBe(activeItem);
+  });
+
+  it(`should remove activeItem`, () => {
+    const wrapper = shallow(<MockComponentWithActiveItem/>);
+    wrapper.props().onActiveItemChange(`4`);
+    wrapper.props().onActiveItemRemoval();
+    expect(wrapper.props().activeItem).toBe(null);
+  });
+});
