94a65661df5eb348cba49bff0d52879ae65c04fc
kopniiiin <kopniiiin@icloud.com>
2020-07-29 22:10:03 +0300

    Добавляет экран избранных предложений

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,10 +6,12 @@ import PropTypes from "prop-types";
 import {City, SortType, Path, AuthorizationStatus} from "../../const.js";
 
 import GuestRoute from "../guest-route/guest-route.jsx";
+import PrivateRoute from "../private-route/private-route.jsx";
 import Header from "../header/header.jsx";
 import Main from "../main/main.jsx";
 import LoginScreen from "../login-screen/login-screen.jsx";
 import OfferScreen from "../offer-screen/offer-screen.jsx";
+import FavoritesScreen from "../favorites-screen/favorites-screen.jsx";
 
 import withActiveItem from "../../hocs/with-active-item/with-active-item.jsx";
 import withAuthorizationData from "../../hocs/with-authorization-data/with-authorization-data.jsx";
@@ -23,12 +25,6 @@ import {getFilteredAndSortedOffers} from "../../reducer/offers/selectors.js";
 
 const MainWithActiveItem = withActiveItem(Main);
 const LoginScreenWithAuthorizationData = withAuthorizationData(LoginScreen);
-
-const Screen = {
-  MAIN: `MAIN`,
-  LOGIN: `LOGIN`,
-  OFFER: `OFFER`
-};
 
 const offerScreenPropTypesCopy = Object.assign({}, OfferScreen.propTypes);
 offerScreenPropTypesCopy.id = PropTypes.string.isRequired;
@@ -97,6 +93,13 @@ class App extends PureComponent {
               onOfferCardBookmarkButtonClick={this._handleOfferCardBookmarkButtonClick}>
               {header}
             </OfferScreen>
+          )}/>
+
+          <PrivateRoute authorizationStatus={authorizationStatus} path={Path.FAVORITES} exact render={() => (
+            <FavoritesScreen
+              onOfferCardBookmarkButtonClick={this._handleOfferCardBookmarkButtonClick}>
+              {header}
+            </FavoritesScreen>
           )}/>
 
         </Switch>
