c1f0ebd2b3b99c9e28e6444f225ef900716f7934
kopniiiin <kopniiiin@icloud.com>
2020-08-04 01:04:40 +0300

    Настраивает TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require(`path`);
 
 module.exports = {
-  entry: `./src/index.js`,
+  entry: `./src/index.tsx`,
   output: {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
@@ -19,8 +19,14 @@ module.exports = {
         use: {
           loader: `babel-loader`
         }
+      }, {
+        test: /\.(ts|tsx)$/,
+        loader: `ts-loader`
       }
     ]
+  },
+  resolve: {
+    extensions: [`.js`, `.ts`, `.tsx`, `json`]
   },
   devtool: `source-map`
 };
